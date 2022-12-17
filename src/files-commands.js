d0187554f7f6cc2cdc2b84fed55fb4b469aff95c
surface74 <warwhisper@tut.by>
2022-12-17 21:50:08 +0300

    feat: add copy function

--- src/files-commands.js
+++ src/files-commands.js
@@ -70,14 +70,12 @@ export const rn = (currentPath, args) => {
     if (!path.isAbsolute(sourceFile)) {
       sourceFile = path.join(currentPath, sourceFile);
     }
-    console.log('sourceFile: ', sourceFile);
 
     let destinationFile = path.normalize(args[2]);
     if (destinationFile.match(/[\\/:]/)) {
-      reject(new Result(new InvalidArgumentError(), false)); //just file name enabled
+      reject(new Result(new InvalidArgumentError('2nd parameter have to be the file name only'), false));
     }
     destinationFile = path.join(path.dirname(sourceFile), destinationFile);
-    console.log('destinationFile: ', destinationFile);
 
     access(destinationFile, constants.F_OK, (err) => {
       if (!err) {
@@ -95,3 +93,41 @@ export const rn = (currentPath, args) => {
     });
   })
 };
+
+export const copy = (currentPath, args) => {
+  return new Promise((resolve, reject) => {
+    if (args.length < 3) {
+      reject(new Result(new InvalidArgumentError(), false));
+    }
+
+    let sourceFile = path.normalize(args[1]);
+    if (!path.isAbsolute(sourceFile)) {
+      sourceFile = path.join(currentPath, sourceFile);
+    }
+
+    let destinationFile = path.normalize(args[2]);
+    if (!path.isAbsolute(destinationFile)) {
+      destinationFile = path.join(currentPath, destinationFile);
+    }
+    const readStream = new createReadStream(sourceFile, { flags: 'r' });
+    const writeStream = new createWriteStream(destinationFile, { flags: 'wx' });
+
+    readStream.pipe(writeStream);
+
+    writeStream.on('error', error =>
+      reject(new Result(new OperationFailedError(error.message), false)));
+
+    writeStream.on('finish', () => {
+      console.colorLog(color.green, `File ${sourceFile} copied to ${destinationFile}`);
+      resolve(new Result(null, true))
+    });
+
+    readStream.on('error', error =>
+      reject(new Result(new OperationFailedError(error.message), false)));
+
+    // readStream.on('end', () => {
+    //   console.colorLog(color.green, `File ${sourceFile} has been read`);
+    //   resolve(new Result(null, true));
+    // });
+  })
+}
