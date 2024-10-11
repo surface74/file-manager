cb23d48790cf0a4fa76b225e9f95e933a9600cb3
surface74 <surface.msn@gmail.com>
2024-10-11 16:34:10 +0300

    feat: implement navigation:ls

--- src/command/navigation.js
+++ src/command/navigation.js
@@ -24,3 +24,26 @@ export const cd = async ([destination]) => {
   }
 }
 
+export const ls = async () => {
+  try {
+    const dirs = [];
+    const files = [];
+    const entities = await readdir(process.cwd(), { withFileTypes: true });
+    for (const entity of entities) {
+      if (entity.isFile()) {
+        files.push({ Name: entity.name, Type: 'file' });
+      } else if (entity.isDirectory()) {
+        dirs.push({ Name: entity.name, Type: 'directory' })
+      }
+    }
+    dirs.sort((item1, item2) => item1.Name.localeCompare(item2.Name));
+    files.sort((item1, item2) => item1.Name.localeCompare(item2.Name));
+
+    if (dirs.length + files.length)
+      console.table([...dirs, ...files]);
+
+    return makeResult(null, process.cwd());
+  } catch (error) {
+    return makeResult(new OperationFailedError(error.message), null);
+  }
+}
