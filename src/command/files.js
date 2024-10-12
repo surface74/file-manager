070ed3539009a45bb5d81a487caa880688bccc0d
surface74 <surface.msn@gmail.com>
2024-10-13 00:13:34 +0300

    refactor: refine files:rn

--- src/command/files.js
+++ src/command/files.js
@@ -1,12 +1,12 @@
 import * as path from 'node:path';
-import { createReadStream, createWriteStream, constants, access, rename } from 'node:fs';
+import { createReadStream, createWriteStream, constants, access } from 'node:fs';
 import { stdout } from 'node:process';
 
 import { InvalidArgumentError, OperationFailedError } from '../error.js';
 import Message from '../message.js';
 import { getAbsolutePath } from '../utils/path-utils.js';
 import { pipeline } from 'node:stream/promises';
-import { writeFile } from 'node:fs/promises';
+import { stat, writeFile, rename } from 'node:fs/promises';
 
 export const cat = ([readFileName]) => {
   if (!readFileName) {
@@ -33,25 +33,15 @@ export const add = async ([fileName]) => {
   }
 
   let fullPath = path.join(process.cwd(), fileName);
-  // const writeStream = createWriteStream(fullPath, { flags: 'ax' });
 
   try {
     await writeFile(fullPath, '', { flag: 'wx' });
   } catch (error) {
     throw new OperationFailedError(error.message);
   }
-  // return new Promise((resolve, reject) => {
-  //   const writeStream = createWriteStream(fullPath, { flags: 'ax' });
-
-  //   writeStream.on('error', error => reject(new OperationFailedError(error.message)));
-  //   writeStream.on('ready', () => {
-  //     writeStream.close();
-  //     resolve(true);
-  //   });
-  // })
 };
 
-export const rn = ([originName, resultName]) => {
+export const rn = async ([originName, resultName]) => {
   if (!originName || !resultName) {
     throw new InvalidArgumentError(Message.NEED_2_ARGS);
   }
@@ -63,20 +53,17 @@ export const rn = ([originName, resultName]) => {
   let sourceFile = getAbsolutePath(originName);
   let destinationFile = path.join(path.dirname(sourceFile), resultName);
 
-  return new Promise((resolve, reject) => {
-    access(destinationFile, constants.F_OK, error => {
-      if (!error) {
-        reject(new InvalidArgumentError(Message.FILE_EXISTS.replace('%%FILE_EXISTS%%', destinationFile)));
-      } else {
-        rename(sourceFile, destinationFile, error => {
-          if (error) {
-            reject(new OperationFailedError(error.message));
-          }
-          resolve(null);
-        });
-      }
-    });
-  })
+  try {
+    await stat(destinationFile);
+  } catch (error) {
+    try {
+      await rename(sourceFile, destinationFile);
+    } catch (error) {
+      throw new OperationFailedError(error.message);
+    }
+    return;
+  }
+  throw new OperationFailedError(Message.FILE_EXISTS.replace('%%FILE%%', destinationFile));
 };
 
 export const cp = async ([source, destination]) => {
